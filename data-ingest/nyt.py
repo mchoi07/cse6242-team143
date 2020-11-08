948e6c63f068e585e23e18afb31828f26fc0f488
Prince O. Onyenike <codeyans@gmail.com>
2020-11-08 20:04:14 +0100

    Update nyt.py

--- data-ingest/nyt.py
+++ data-ingest/nyt.py
@@ -1,8 +1,23 @@
 import urllib.request
 import json
+from nltk.corpus import stopwords
+from textblob import TextBlob, Word
 
-APP_KEY = 'API KEY HERE'
+APP_KEY = 'APP KEY HERE'
 
+tag_dict = {
+    "J": 'a', 
+    "N": 'n', 
+    "V": 'v', 
+    "R": 'r'
+}
+
+def clean_string(sentence):
+    sentence = TextBlob(sentence)
+    words_and_tags = [(w, tag_dict.get(pos[0], 'n')) for w, pos in sentence.tags]
+    lematized_list = [wd.lemmatize(tag) for wd, tag in words_and_tags if wd not in stopwords.words('english')]
+    
+    return " ".join(lematized_list)
 
 def get_data(year=2020, num_of_months=2):
     '''
@@ -19,9 +34,9 @@ def get_data(year=2020, num_of_months=2):
     '''
     result_json = []
 
-    apple_keywords = ['iphone', 'iphones', 'apple', 'ipad', 'macbook', 'airpod']
-    tesla_keywards = ['electric battery', 'model3', 'electric car', 'electric vehicle', 'hyperloop', 'tesla']
-    netflix_keywards = ['netflix']
+    apple_keywords = ['iphone', 'iphones', 'apple', 'ipad', 'macbook', 'airpod', 'ios', 'macos', 'tim cook']
+    tesla_keywards = ['electric battery', 'model3', 'electric car', 'electric vehicle', 'hyperloop', 'tesla', 'self driving', 'autonomous vehicle', 'spacex', 'elon musk', 'solarcity']
+    netflix_keywards = ['netflix', 'reed hastings']
     disney_keywords = ['disney']
 
     for idx in range(1, num_of_months):
@@ -53,6 +68,7 @@ def get_data(year=2020, num_of_months=2):
 
                 doc_simple = {
                     'abstract': doc['abstract'],
+                    'abstract_clean': clean_string(doc['abstract']),
                     'snippet': doc['snippet'],
                     'main_headline': doc['headline']['main'],
                     'name_headline': doc['headline']['name'],
